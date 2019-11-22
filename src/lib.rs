ade8eca3a6f67a751cdaf0a400929a98f3ee42ed
Chad Laing <chadlaing@inoutbox.com>
2019-11-22 10:33:19 -0700

    Add empty files for testing lib

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,80 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+
+use std::io::{Error, ErrorKind};
+use std::path::{Path, PathBuf};
+use std::{fs, io};
+
+/// Returns a vector of all files in a given path (file or directory)
+/// Optionally takes a vector of extensions to match agains
+/// # Examples
+///
+/// # /dir/fasta1.fasta
+/// # /dir/fasta2.fasta
+/// # /dir/not_fasta.txt
+///
+/// ```
+///
+
+/// ```
+///
+pub fn get_fasta_path(file_or_dir: &Path) -> Result<Vec<PathBuf>, Error> {
+    if file_or_dir.is_file() {
+        Ok(vec![file_or_dir.to_path_buf()])
+    } else if file_or_dir.is_dir() {
+        let all_files = recurse_directory(file_or_dir)?;
+
+        // Of all the files, we want to keep only the fasta files
+        let mut fasta_files: Vec<PathBuf> = Vec::new();
+
+        // To ensure we only get fasta files, we open each file, and attempt
+        // to get the first fasta record of each. If we succeed, we add the file to
+        // the vector of fasta files. If not, we do nothing.
+        for f in all_files {
+            let reader = fasta::Reader::from_file(&f);
+            match reader {
+                Ok(gf) => {
+                    for record in gf.records() {
+                        match record {
+                            Ok(..) => fasta_files.push(f),
+                            Err(..) => {}
+                        }
+                        break;
+                    }
+                }
+                Err(..) => {}
+            }
+        }
+
+        // Check to see if any fasta files were found. If not, return an error
+        if fasta_files.is_empty() {
+            Err(Error::new(
+                ErrorKind::NotFound,
+                "No valid fasta files found",
+            ))
+        } else {
+            Ok(fasta_files)
+        }
+    } else {
+        Err(Error::new(ErrorKind::NotFound, "No valid files found"))
+    }
+}
+
+// Path only holds a reference to the path string
+// PathBuf owns the string
+fn recurse_directory(p: &Path) -> Result<Vec<PathBuf>, Error> {
+    let mut af: Vec<PathBuf> = Vec::new();
+    for entry in fs::read_dir(p)? {
+        let e = entry?;
+        let path = e.path();
+
+        if path.is_dir() {
+            recurse_directory(&path)?;
+        } else {
+            af.push(path);
+        }
+    }
+    Ok(af)
+}
