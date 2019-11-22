7909625fe4f330ccb8dabcd6d96b16a54b375f66
Chad Laing <chadlaing@inoutbox.com>
2019-11-22 16:00:34 -0700

    Ready for library use

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
+use std::collections::HashMap;
+use std::fs;
 use std::io::{Error, ErrorKind};
 use std::path::{Path, PathBuf};
-use std::{fs};
-
+use std::ffi::OsStr;
 
 #[cfg(test)]
 mod tests {
@@ -12,69 +13,70 @@ mod tests {
         let mut fs = file_or_dirfiles(Path::new("data/"), None).unwrap();
         fs.sort();
 
-        assert_eq!(fs, vec!(
-            Path::new("data/file1.fasta"),
-            Path::new("data/file1.txt"),
-            Path::new("data/file2.fna"),
-            Path::new("data/file2.txt")
-        ));
+        assert_eq!(
+            fs,
+            vec!(
+                Path::new("data/file1.fasta"),
+                Path::new("data/file1.txt"),
+                Path::new("data/file2.fna"),
+                Path::new("data/file2.txt")
+            )
+        );
     }
 
     #[test]
-    fn no_files(){
-        let fs = file_or_dirfiles(Path::new(""),None);
+    fn no_files() {
+        let fs = file_or_dirfiles(Path::new(""), None);
         assert!(fs.is_err());
     }
-}
 
+    #[test]
+    fn only_txt() {
+        let mut fs = file_or_dirfiles(Path::new("data"), Some(vec!["txt"])).unwrap();
+        fs.sort();
 
-
-/// Returns a vector of all files in a given path (file or directory)
-/// Optionally takes a vector of extensions to match agains
-/// # Examples
-///
-/// # /dir/fasta1.fasta
-/// # /dir/fasta2.fasta
-/// # /dir/not_fasta.txt
-///
-/// ```
-///
-
-/// ```
-///
-pub fn file_or_dirfiles(file_or_dir: &Path, file_types: Option<Vec<&str>>) -> Result<Vec<PathBuf>, Error> {
-    if file_or_dir.is_file() {
-
-        // If there was a supplied vec of extensions, match against them
-        Ok(vec![file_or_dir.to_path_buf()])
-    } else if file_or_dir.is_dir() {
-        let all_files = recurse_directory(file_or_dir)?;
-        Ok(all_files)
-    } else {
-        Err(Error::new(ErrorKind::NotFound, "No valid files found"))
+        assert_eq!(
+            fs,
+            vec!(Path::new("data/file1.txt"), Path::new("data/file2.txt"))
+        )
     }
 }
-        // Of all the files, we want to keep only the fasta files
-//        let mut subset_files: Vec<PathBuf> = Vec::new();
 
-        // To ensure we only get fasta files, we open each file, and attempt
-        // to get the first fasta record of each. If we succeed, we add the file to
-        // the vector of fasta files. If not, we do nothing.
-//        for f in all_files {
-//
-//        }
+// Returns a vector of all files in a given path (file or directory)
+// Optionally takes a vector of extensions to match against
+pub fn file_or_dirfiles(
+    file_or_dir: &Path,
+    filter_extensions: Option<Vec<&str>>,
+) -> Result<Vec<PathBuf>, Error> {
+    let mut all_files: Vec<PathBuf> = Vec::new();
 
-        // Check to see if any fasta files were found. If not, return an error
-//        if subset_files.is_empty() {
-//            Err(Error::new(
-//                ErrorKind::NotFound,
-//                "No valid files of type found",
-//            ))
-//        } else {
-//            Ok(subset_files)
-//        }
+    if file_or_dir.is_file() {
+        all_files.push(file_or_dir.to_path_buf());
+    } else if file_or_dir.is_dir() {
+        all_files = recurse_directory(file_or_dir)?;
+    } else {
+        return Err(Error::new(ErrorKind::NotFound, "Not a valid file or directory"));
+    }
 
+    // If there is a filter on file extensions, apply it here
+    if filter_extensions.is_some() {
+        // create a HashMap to test existence of extension
+        let mut all_filters = HashMap::new();
+        for filt in filter_extensions.unwrap() {
+            // It is cheaper to convert a &str to OsStr than the reverse
+            all_filters.insert(OsStr::new(filt), 1);
+        }
 
+        all_files.retain(|x| all_filters.contains_key(x.extension().unwrap()));
+    }
+
+    // Check that we have files for use
+    if all_files.is_empty(){
+        return Err(Error::new(ErrorKind::NotFound, "No valid files found"));
+    }
+
+    Ok(all_files)
+}
 
 // Path only holds a reference to the path string
 // PathBuf owns the string
